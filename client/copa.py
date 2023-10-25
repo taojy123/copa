305c8d8950c4a7b90d63259ca591083cc8f82dbb
taojy123 <taojy123@163.com>
2023-10-25 23:14:33 +0800

    update

---
+++ client/copa.py
@@ -0,0 +1,17 @@
+import datetime
+import hashlib
+import json
+import os
+import sys
+import time
+import traceback
+
+import requests
+import pyperclip
+
+
+from trans import set_language, translate as _
+
+
+
+

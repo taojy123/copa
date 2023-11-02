ff35d22cb15906b07573c96820ef45517a1eed5f
taojy123 <taojy123@163.com>
2023-11-02 23:14:54 +0800

    update

--- client/copa.py
+++ client/copa.py
@@ -13,5 +13,11 @@ import pyperclip
 from trans import set_language, translate as _
 
 
+def md5(content):
+    if not content:
+        return ''
+    return hashlib.md5(content.encode()).hexdigest()
+
+
 
 

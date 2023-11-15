159220835b40590818948d416bcab4ee509e6ccb
taojy123 <taojy123@163.com>
2023-11-15 23:15:11 +0800

    update

--- client/copa.py
+++ client/copa.py
@@ -19,5 +19,21 @@ def md5(content):
     return hashlib.md5(content.encode()).hexdigest()
 
 
+def api_request(conf, method, uri, data=None):
+    host = conf['host']
+    http_proxy = conf['http_proxy']
+    https_proxy = conf['https_proxy']
+    if http_proxy:
+        proxies = {'http': http_proxy, 'https': https_proxy or http_proxy}
+    else:
+        proxies = None
+    url = host + uri
+    r = requests.request(method, url, data=data, proxies=proxies, verify=False)
+    if r.status_code is not 200:
+        print('WARNING:', r.text)
+        return None
+    return r.text
+
+
 
 

9a42c01657c27a151c046d4d812726d88eccb660
taojy123 <taojy123@163.com>
2024-01-15 23:19:00 +0800

    update

---
+++ server/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+if [[ -z "$1" ]]; then
+    echo "run server";
+    python manage.py migrate;
+    python manage.py collectstatic --noinput;
+    gunicorn -w 5 -b 0.0.0.0:8000 --access-logfile=- --error-logfile=- copa.wsgi;
+elif [[ "$1" = "migrate" ]]; then
+    echo "run migrate";
+    python manage.py migrate
+else
+    echo "$@"
+    exec "$@"
+fi
+
+

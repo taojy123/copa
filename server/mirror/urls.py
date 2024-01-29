e508937aed3cd0d35c57ebb6965e7a56082cae33
taojy123 <taojy123@163.com>
2024-01-29 23:20:14 +0800

    update

---
+++ server/mirror/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+from mirror import views
+
+app_name = 'mirror'
+
+
+urlpatterns = [
+    path('clipboards/<token>/', views.clipboard),
+    path('clear', views.clear),
+]
